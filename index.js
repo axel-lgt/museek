5b19f75442829d98b0485750fd623675f6ce4196
Axel LEGENTIL <axel.legentil@outlook.fr>
2022-05-04 16:07:40 +0200

    started fetching with axios in console

--- index.js
+++ index.js
@@ -4,11 +4,32 @@ const dropdown = document.querySelector('.dropdown');
 const dropdownHeader = document.querySelector('.museek-info-header');
 const dropdownContent = document.querySelector('.museek-info-dropdown-content');
 
-search.addEventListener('change', (e) => {
-    e.preventDefault();
-    console.log(e.target.value);
-    songInfo.style.display = 'flex';
-})
+console.log('In index.js');
+
+const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'genius.p.rapidapi.com',
+      'X-RapidAPI-Key': '86fe6b7424msh2f92ac0a32a7924p1f2085jsn203e9baf4b1a'
+    }
+};
+
+const getQuery = async () => {
+    const searchQuery = search.value;
+    console.log('This is the query: ' + searchQuery);
+
+    try {
+        const response = await axios.request(`https://genius.p.rapidapi.com/search?q=${searchQuery}`, options);
+        console.log(response.data);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const registerEventHandler = () => {
+    search.addEventListener('change', getQuery)
+}
+
 
 dropdownHeader.addEventListener('click', () => {
     dropdownHeader.classList.toggle('museek-info-header-clicked');
@@ -16,4 +37,4 @@ dropdownHeader.addEventListener('click', () => {
     dropdownContent.classList.toggle('museek-info-dropdown-content-open');
 })
 
-console.log('In index.js');
+registerEventHandler();
